1e0741fe2a54d3fd587f88deb173bf39b0246110
C_wangchenguang <chenguang.wang2@pactera.com>
2023-10-20 18:05:57 +0800

    1020

---
+++ rabbitmq/go.mod
@@ -0,0 +1,27 @@
+module rabbitmq
+
+go 1.19
+
+require (
+	git.tencent.com/trpc-go/trpc-go v0.10.0
+	github.com/streadway/amqp v1.1.0
+)
+
+require (
+	git.tencent.com/jce/jce v1.2.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/google/flatbuffers v2.0.0+incompatible // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/lestrrat-go/strftime v1.0.3 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/atomic v1.6.0 // indirect
+	go.uber.org/multierr v1.5.0 // indirect
+	go.uber.org/zap v1.16.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
+)
--- rabbitmq/simple.go
+++ rabbitmq/simple.go
@@ -1,3 +1,121 @@
 package main
 
+import (
+	"fmt"
+	tlog "git.tencent.com/trpc-go/trpc-go/log"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
 const mqURL = "amqp://wangShao:wangShao@106.52.228.207:5672/wangShao"
+
+type RabbitMq struct {
+	conn    *amqp.Connection
+	channel *amqp.Channel
+	// 队列名称
+	QueueName string
+	// 交换机
+	Exchange string
+	// Key
+	Key string
+	// 连接信息
+	MqURL string
+}
+
+func NewRabbitMq(queueName string, exchange string, key string, mqURL string) *RabbitMq {
+	rabbitMq := &RabbitMq{QueueName: queueName, Exchange: exchange, Key: key, MqURL: mqURL}
+
+	var err error
+	rabbitMq.conn, err = amqp.Dial(rabbitMq.MqURL)
+	rabbitMq.failOnErr(err, "create conn err!")
+	rabbitMq.channel, err = rabbitMq.conn.Channel()
+	rabbitMq.failOnErr(err, "get channel failed!")
+
+	return rabbitMq
+}
+
+func (mq *RabbitMq) Destory() {
+	mq.conn.Close()
+	mq.channel.Close()
+}
+
+// failOnErr err detail
+func (mq *RabbitMq) failOnErr(err error, message string) {
+	if err != nil {
+		log.Fatalf("%s:%s", message, err)
+		panic(fmt.Sprintf("%s:%s", message, err))
+	}
+}
+
+// NewRabbitMqSimple queueName队列名称
+func NewRabbitMqSimple(queueName string) *RabbitMq {
+	// exchange为""，默认使用default
+	return NewRabbitMq(queueName, "", "", mqURL)
+}
+
+// PublishSimple Simple模式推送消息  生产者
+func (mq *RabbitMq) PublishSimple(message string) {
+	//申请队列，如果队列不存在会自动创建，如果存在则跳过创建
+	//保证队列存在，消息能发送到队列中
+	_, err := mq.channel.QueueDeclare(
+		mq.QueueName, // 队列名称
+		false,        // 是否持久化
+		false,        // 是否为自动删除
+		false,        // 是否具有排他性，不常用，创建一个自己可见的队列，其他用户不可见
+		false,        // 是否阻塞，发送消息后是否等待响应
+		nil,          // 额外属性
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// 发送消息到队列中
+	err = mq.channel.Publish(
+		mq.Exchange,  // 为""则使用default队列
+		mq.QueueName, // 队列名称
+		// 如果为true，根据exchange类型和routkey规则，如果无法找到符合条件的队列那么会把发送的消息返回给发送者
+		false,
+		// 如果为true,当exchange发送消息到队列后发现没有绑定消费者，则会把消息返还给发送者
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		},
+	)
+	if err != nil {
+		tlog.Errorf("mq simple publish err: %s", err)
+	}
+}
+
+// ConsumeSimple Simple模式接收消息  消费者
+func (mq *RabbitMq) ConsumeSimple(message string) {
+	//申请队列，如果队列不存在会自动创建，如果存在则跳过创建
+	//保证队列存在，消息能发送到队列中
+	_, err := mq.channel.QueueDeclare(
+		mq.QueueName, // 队列名称
+		false,        // 是否持久化
+		false,        // 是否为自动删除
+		false,        // 是否具有排他性，不常用，创建一个自己可见的队列，其他用户不可见
+		false,        // 是否阻塞，发送消息后是否等待响应
+		nil,          // 额外属性
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// 发送消息到队列中
+	err = mq.channel.Publish(
+		mq.Exchange,  // 为""则使用default队列
+		mq.QueueName, // 队列名称
+		// 如果为true，根据exchange类型和routkey规则，如果无法找到符合条件的队列那么会把发送的消息返回给发送者
+		false,
+		// 如果为true,当exchange发送消息到队列后发现没有绑定消费者，则会把消息返还给发送者
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		},
+	)
+	if err != nil {
+		tlog.Errorf("mq simple publish err: %s", err)
+	}
+}
